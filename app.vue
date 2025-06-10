081da59245b31e84dbb28218e2eefa624fb8f0cd
Anastasiya Volokitina <n060600@yandex.ru>
2025-06-10 13:28:13 +0500

    upd

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 const router = useRouter();
 const route = useRoute();
 
-function goBack () {
+function goBack() {
   window.scrollTo({top: 0, behavior: 'smooth'});
   if (window?.history.state.back === null) {
     router.push('https://anastasiy-alt.github.io/animation-nuxt')
@@ -10,6 +10,7 @@ function goBack () {
     window?.history.back()
   }
 }
+
 const buttonIsVisible = ref(false)
 
 const handleScroll = () => {
@@ -36,15 +37,20 @@ onUnmounted(() => {
 
 <template>
   <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Electrolize&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Zen+Dots&display=swap"
-          rel="stylesheet">
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link crossorigin href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Electrolize&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Zen+Dots&display=swap"
+        rel="stylesheet">
   </head>
   <div class="page">
-    <button class="back-button custom-button" @click="goBack" v-if="route.fullPath !== '/'">Go back</button>
+    <NuxtLink v-if="route.fullPath !== '/'" class="back-button custom-button"
+              to="https://anastasiy-alt.github.io/animation-nuxt">Go back
+    </NuxtLink>
     <NuxtPage/>
-    <button :class="[{'top-button_visible': buttonIsVisible}]"  class="top-button custom-button" @click="scrollToTop">Top!</button>
+    <button :class="[{'top-button_visible': buttonIsVisible}]" class="top-button custom-button" @click="scrollToTop">
+      Top!
+    </button>
   </div>
 
 </template>
@@ -55,24 +61,35 @@ onUnmounted(() => {
   font-style: normal
   margin: 0
   padding: 0
+
 h1, h2
   text-align: center
   margin: 50px auto
   color: #e6e6e6
+
 p
   color: #e6e6e6
+
 .page
   margin: 50px 100px
+
 body
   background-color: #252423
 
+.custom-button:visited
+  text-decoration: none !important
+  color: black !important
+
 .custom-button
+  text-decoration: none !important
+  color: black !important
   background: #e6e6e6
   border: 5px solid #6aff65
   border-radius: 100px
   padding: 10px 20px
   cursor: pointer
   transition: .35s all
+
 .custom-button:hover
   background: white
   transform: scale(1.1)
@@ -90,6 +107,7 @@ body
   right: 180px
   visibility: hidden
   z-index: 13
+
 .top-button_visible
   opacity: 1
   visibility: visible
