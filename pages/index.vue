7ad4e811c4da40ff6d6ecfb81e0db619ed485d42
Anastasiya Volokitina <n060600@yandex.ru>
2025-06-10 13:12:00 +0500

    test

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,16 @@
   <h2>The links show pages with usage examples.</h2>
   <ul>
     <li>
-      <NuxtLink class="link" to="/animation-nuxt/main">Main display</NuxtLink>
+      <NuxtLink class="link" to="https://anastasiy-alt.github.io/animation-nuxt/main">Main display</NuxtLink>
     </li>
     <li>
-      <NuxtLink class="link" to="/animation-nuxt/svg">Svg animations</NuxtLink>
+      <NuxtLink class="link" to="https://anastasiy-alt.github.io/animation-nuxt/svg">Svg animations</NuxtLink>
     </li>
     <li>
-      <NuxtLink class="link" to="/animation-nuxt/simple">Simple</NuxtLink>
+      <NuxtLink class="link" to="https://anastasiy-alt.github.io/animation-nuxt/simple">Simple</NuxtLink>
     </li>
     <li>
-      <NuxtLink class="link" to="/animation-nuxt/scroll">Scroll</NuxtLink>
+      <NuxtLink class="link" to="https://anastasiy-alt.github.io/animation-nuxt/scroll">Scroll</NuxtLink>
     </li>
 
   </ul>
