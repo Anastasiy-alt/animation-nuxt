1ce03b9930d442a01962eb803b0371f9b673f6d1
Anastasiya Volokitina <n060600@yandex.ru>
2025-06-18 13:19:24 +0500

    variant two - all in

---
+++ components/main/variant/two/cards.vue
@@ -0,0 +1,111 @@
+<script setup>
+onMounted(() => {
+  const cards = document.querySelectorAll('.card')
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        cards.forEach(el => el.classList.add('anim'))
+        observer.unobserve(entry.target)
+      }
+    });
+  }, {
+    root: null,
+    threshold: 0.5,
+  });
+  observer.observe(document.querySelector('.card-container'));
+})
+
+</script>
+
+
+<template>
+  <section class="card-container">
+    <div class="card">1</div>
+    <div class="card">2</div>
+    <div class="card">3</div>
+    <div class="card">4</div>
+    <div class="card">5</div>
+    <div class="card">6</div>
+    <div class="card">7</div>
+    <div class="card">8</div>
+  </section>
+</template>
+
+<style lang="sass" scoped>
+@use "/const/color"
+
+.card-container
+  position: relative
+  margin-top: -600px
+  height: 1000px
+  overflow: hidden
+
+.card
+  position: absolute
+  top: 200px
+  left: 0
+  width: 300px
+  height: 400px
+  border-radius: 1rem
+  background-color: color.$light-green
+  transform: translateY(100vh)
+.card.anim
+  animation-name: anim-card
+  animation-duration: 1s
+  animation-fill-mode: forwards
+.card:nth-child(2)
+  left: 400px
+  top: 0
+  &.anim
+    animation-delay: .5s
+
+.card:nth-child(3)
+  left: 800px
+  top: 100px
+  &.anim
+    animation-delay: 1.2s
+
+.card:nth-child(4)
+  left: auto
+  right: 350px
+  top: 300px
+  &.anim
+    animation-delay: 1.5s
+
+.card:nth-child(5)
+  left: auto
+  right: 0
+  top: 200px
+  &.anim
+    animation-delay: 2.2s
+
+.card:nth-child(6)
+  left: 150px
+  top: 420px
+  &.anim
+    animation-delay: 2.8s
+
+.card:nth-child(7)
+  left: 550px
+  top: 550px
+  &.anim
+    animation-delay: 3.1s
+
+.card:nth-child(8)
+  left: auto
+  right: 500px
+  top: 600px
+  &.anim
+    animation-delay: 3.5s
+
+.card:nth-child(3n)
+  background-color: color.$white
+
+.card:nth-child(3n + 1)
+  background-color: color.$light-yellow
+@keyframes anim-card
+  0%
+    transform: translateY(100vh)
+  100%
+    transform: translateY(0)
+</style>
---
+++ components/main/variant/two/tardis.vue
@@ -0,0 +1,196 @@
+<script setup>
+import {animate, onScroll} from "animejs";
+
+onMounted(() => {
+  animate('.scroll-block', {
+    width: '100%',
+    opacity: 1,
+    duration: 1000,
+    alternate: true,
+    ease: 'inOutQuad',
+    autoplay: onScroll({
+      sync: 0.5,
+      enter: 'top-=10% top',
+      leave: 'top-=600 top',
+      debug: false
+    })
+  });
+
+  animate('.inner__title-block', {
+    // y: '-100px',
+    opacity: 1,
+    duration: 1000,
+    alternate: true,
+    ease: 'inOutQuad',
+    autoplay: onScroll({
+      sync: 0.5,
+      enter: 'top-=300 top',
+      leave: 'top-=500 top',
+      debug: false
+    })
+  });
+
+  animate('.inner__info-block', {
+    height: '400px',
+    opacity: 1,
+    duration: 1000,
+    alternate: true,
+    ease: 'inOutQuad',
+    autoplay: onScroll({
+      sync: 0.5,
+      enter: 'top-=600 top',
+      leave: 'top-=800 top',
+      debug: false
+    })
+  });
+  animate('.inner__img-block', {
+    opacity: 1,
+    duration: 1000,
+    height: '400px',
+    alternate: true,
+    ease: 'inOutQuad',
+    autoplay: onScroll({
+      sync: 0.5,
+      enter: 'top-=900 top',
+      leave: 'top-=1100 top',
+      debug: false
+    })
+  });
+  animate('.inner__img_second', {
+    duration: 1000,
+    x: '-300px',
+    alternate: true,
+    ease: 'inOutQuad',
+    autoplay: onScroll({
+      sync: 0.5,
+      enter: 'top-=1800 top',
+      leave: 'top-=2200 top',
+      debug: false
+    })
+  });
+})
+</script>
+
+<template>
+  <section class="scroll-block">
+    <div class="scroll-block__inner">
+      <div class="scroll-block__inner-content inner">
+        <div class="inner__title-block">
+          <p class="inner__title">The TARDIS</p>
+          <p class="inner__subtitle">Vworp!</p>
+        </div>
+        <div class="inner__info-block">
+          <p class="inner__info">The TARDIS is the Doctor's ship, their home and their friend. It has been the one
+            constant through all their lives; an impossible blue box, bigger on the inside than the outside, not
+            necessarily taking them where they want to go, but very often where they need to be. It can reach any time
+            or place, and with it the Doctor has shown the universe to their many, many friends.
+            <br><br>
+            ‘TARDIS’ stands for ‘Time and Relative Dimension in Space’ – an acronym coined by Susan, the Doctor’s
+            granddaughter. A Type 40 capsule created by the Time Lords, the TARDIS is the Doctor’s incredible ship for
+            travelling in time and space. The First Doctor actually stole the TARDIS, using it to run away and embark on
+            adventures across the universe. Or, perhaps, was it the TARDIS that stole him?
+          </p>
+          <div class="inner__img-block">
+            <img alt="" class="inner__img" src="~/assets/main/tardis.jpg">
+            <img alt="" class="inner__img inner__img_second" src="~/assets/main/tardis-inside.jpg">
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="sass" scoped>
+@use "/const/color"
+
+.inner__title-block
+  opacity: 0
+
+.inner__info-block
+  height: 0
+  opacity: 0
+
+.inner__title-block
+  display: flex
+  flex-direction: column
+  gap: 10px
+  border-bottom: 2px solid color.$grey
+
+.inner__title
+  white-space: nowrap
+  font-size: 28px
+  color: black
+
+.inner__subtitle
+  font-size: 24px
+  color: black
+
+.inner__info
+  font-size: 24px
+  color: black
+  height: 100%
+  overflow: hidden
+
+.inner__info-block
+  display: flex
+  gap: 50px
+  flex-direction: row
+
+.inner__img
+  position: absolute
+  top: 0
+  right: 0
+  width: 100%
+  height: 100%
+  object-fit: cover
+  object-position: center
+
+.inner__img_second
+  right: -300px
+
+.inner__img-block
+  position: relative
+  opacity: 0
+  overflow: hidden
+  width: 300px
+  min-width: 300px
+  height: 1px
+  border-radius: 20px
+
+.scroll-block
+  opacity: 0
+  height: 450vh
+  width: 0
+  margin: 0 auto
+
+.scroll-block__inner
+  position: sticky
+  top: 0
+  display: flex
+  justify-content: center
+  align-items: center
+  width: 60%
+  margin: 0 auto
+  height: 100vh
+
+
+.scroll-block__inner-content
+  border-radius: 50px
+  width: 100%
+  height: fit-content
+  max-height: 80vh
+  background: color.$white
+  outline: 5px solid color.$grey
+  padding: 30px
+  box-sizing: border-box
+  display: flex
+  flex-direction: column
+  gap: 30px
+
+.scroll-block__image
+  width: 100%
+  height: 100%
+  object-fit: cover
+
+</style>
